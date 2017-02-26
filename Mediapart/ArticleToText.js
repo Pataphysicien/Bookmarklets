2ff32dfa37662817f168aa3a40e43b2fe5fec2c8
ma.coutanceau <ma.coutanceau@ideine.fr>
2017-02-26 14:27:04 +0100

    First implementation, all the code is in the bookmarklet

--- Mediapart/ArticleToText.js
+++ Mediapart/ArticleToText.js
@@ -1,20 +1,11 @@
-console.debug("ArticleToText.js is loaded and being executed.");
-(function (undefined) {
+/* Removes all but text of an article on www.mediapart.fr */
+javascript: (function removeAllButText(undefined) {
     'use strict';
-    console.debug("articleToTextJsMain called");
-
+    console.debug('loadFromBookMarklet called');
+    $("body").children().not("section.global-wrapper").hide();
+    $(".read-also").hide();
+    $(".tools-share").hide();
+    $("aside").hide();
+    $(".media").hide();
+    $(".sub-menu").hide();
 })();
-
-/* Put this code in a bookmark 
-    javascript: (function loadFromBookMarklet(undefined) {
-        'use strict';
-        console.debug('loadFromBookMarklet called');
-        var fullJsUri = 'https://raw.githubusercontent.com/Pataphysicien/Bookmarklets/master/Mediapart/ArticleToText.js?v=' + parseInt(Math.random() * 99999999);
-        console.debug('loading from address ', fullJsUri);
-
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src  = fullJsUri;
-        document.getElementsByTagName('head')[0].appendChild(script);
-    })();
-*/
