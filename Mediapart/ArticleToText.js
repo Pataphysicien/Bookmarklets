6e356c397bb180f13f096f6f0b44fd91b291c16c
ma.coutanceau <ma.coutanceau@ideine.fr>
2017-02-26 13:45:16 +0100

    Adding bookmarklet code (as comments) and adding some console.debg() traces

--- Mediapart/ArticleToText.js
+++ Mediapart/ArticleToText.js
@@ -1,5 +1,20 @@
-(function articleToTextJsMain(undefined) {
+console.debug("ArticleToText.js is loaded and being executed.");
+(function (undefined) {
     'use strict';
     console.debug("articleToTextJsMain called");
 
 })();
+
+/* Put this code in a bookmark 
+    javascript: (function loadFromBookMarklet(undefined) {
+        'use strict';
+        console.debug('loadFromBookMarklet called');
+        var fullJsUri = 'https://raw.githubusercontent.com/Pataphysicien/Bookmarklets/master/Mediapart/ArticleToText.js?v=' + parseInt(Math.random() * 99999999);
+        console.debug('loading from address ', fullJsUri);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src  = fullJsUri;
+        document.getElementsByTagName('head')[0].appendChild(script);
+    })();
+*/
